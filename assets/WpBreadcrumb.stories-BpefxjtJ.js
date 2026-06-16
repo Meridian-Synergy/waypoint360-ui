7166052c3133e1d29c6992d9b3cce1f0f2ce4238
dgosset <dgosset@users.noreply.github.com>
2026-06-16 16:00:12 +0000

    deploy: a8907e07992d54a36d0dace64f4911bd5276cb55

--- assets/WpBreadcrumb.stories-BpefxjtJ.js
+++ assets/WpBreadcrumb.stories-BpefxjtJ.js
@@ -1,4 +1,4 @@
-import{W as s}from"./WpBreadcrumb-DFONY1B2.js";import"./iframe-CkCqwiia.js";import"./preload-helper-PPVm8Dsz.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const n={title:"Components/WpBreadcrumb",component:s,tags:["autodocs"]},e={args:{crumbs:[{label:"Accueil",href:"/"},{label:"Services"}]}},r={args:{crumbs:[{label:"Accueil",href:"/"},{label:"Services",href:"/services"},{label:"Audit thermique"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+import{W as s}from"./WpBreadcrumb-BVnukwkx.js";import"./iframe-BEQMzRqY.js";import"./preload-helper-PPVm8Dsz.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const n={title:"Components/WpBreadcrumb",component:s,tags:["autodocs"]},e={args:{crumbs:[{label:"Accueil",href:"/"},{label:"Services"}]}},r={args:{crumbs:[{label:"Accueil",href:"/"},{label:"Services",href:"/services"},{label:"Audit thermique"}]}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
   args: {
     crumbs: [{
       label: 'Accueil',
