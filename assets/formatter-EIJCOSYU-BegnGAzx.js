2c8da52cf18cf8e81edda25bb508c273c5f4adf7
dgosset <dgosset@users.noreply.github.com>
2026-06-17 08:40:10 +0000

    deploy: 6c38117847b3d5b76c1918abf102df5439d6992e

---
+++ assets/formatter-EIJCOSYU-BegnGAzx.js
@@ -0,0 +1,1 @@
+import{d as m,a as e}from"./DocsRenderer-LL677BLK-CnFWqnPa.js";import{b as o}from"./iframe-zW4CPUyb.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
