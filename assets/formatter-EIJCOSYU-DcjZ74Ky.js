448081662cb322e725ca284ece4f44055f90d77a
dgosset <dgosset@users.noreply.github.com>
2026-08-23 21:57:15 +0000

    deploy: c1a100de817768f42002588d2416d34021e6d879

---
+++ assets/formatter-EIJCOSYU-DcjZ74Ky.js
@@ -0,0 +1,1 @@
+import{d as m,a as e}from"./DocsRenderer-LL677BLK-B8vvw_U5.js";import{b as o}from"./iframe-34ht2r-x.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
